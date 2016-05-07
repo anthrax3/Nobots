112d21452e324fd596313401466a8602ed72000f
Marius Lindvall <marius@varden.info>
2016-05-07 22:23:32 +0200

    Added GitHub ribbons

--- src/checkpoint/index.php
+++ src/checkpoint/index.php
@@ -32,6 +32,7 @@ function fillHash() {
 </form>
 <p class="footer">Do not submit private or confidential information using this form. We are<br />not responsible for your data. All submitted data is to be considered public.</p>
 </div>
+<a href="https://github.com/bilde2910/Nobots"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
 </body>
 </html>
 <?php
@@ -87,6 +88,7 @@ function fillHash() {
 <p>The link you were given doesn't work!<br />You can always try asking for another one :-)</p>
 <p class="footer">Do not submit private or confidential information using this form. We are<br />not responsible for your data. All submitted data is to be considered public.</p>
 </div>
+<a href="https://github.com/bilde2910/Nobots"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
 </body>
 </html>
 
@@ -108,6 +110,7 @@ function fillHash() {
 <div class="iwrap"><div class="iinner"><div class="caret">&gt;</div><input onclick="this.select();" type="text" readonly id="nobots" value="<?php echo $decvalue; ?>" /><div class="caret">&#x2714;</div></div></div>
 <p class="footer">Do not submit private or confidential information using this form. We are<br />not responsible for your data. All submitted data is to be considered public.</p>
 </div>
+<a href="https://github.com/bilde2910/Nobots"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
 </body>
 </html>
 <?php
